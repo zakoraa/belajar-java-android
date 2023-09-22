b75d86d6c769f45d73b495e63f095e29d297fc1c
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-09-22 10:48:29 +0700

    learned: using findViewById & setOnClickListener

--- app/src/main/java/com/example/belajar_java_android/MainActivity.java
+++ app/src/main/java/com/example/belajar_java_android/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.belajar_java_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hello_world);
+
+        EditText nameEditText = findViewById(R.id.nameEditText);
+        
+        Button loginButton = findViewById(R.id.doLoginButton);
+
+        TextView loginSuccess = findViewById(R.id.loginSuccessTextView);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                String name = nameEditText.getText().toString();
+                if(name.length() == 0){
+                    loginSuccess.setText("");
+                }else {
+                    loginSuccess.setText("Login Success, Welcome " + name);
+                }
+            }
+        });
+        
+        
     }
 }
